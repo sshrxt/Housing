dc5e6096f4df7a257d856740c572ec20a3fd67b6
Shrut <shrutpatel20@gmail.com>
2024-03-09 23:57:25 -0600

    Fixed bugs

--- main.cpp
+++ main.cpp
@@ -8,12 +8,14 @@ int main(){
     House* house1 = new House("8837 Robin");
     House* house2 = new House("Bay Colony");
     House* house3 = new House("Hollyberry");
+    House* house4 = new House("Bianco Terrace");
 
     HouseList myHouses;
     myHouses.push_front(house1);
     myHouses.push_front(house2);
     myHouses.push_front(house3);
+    myHouses.push_front(house4);
 
-    
+
     myHouses.printAllHouses();
 }
